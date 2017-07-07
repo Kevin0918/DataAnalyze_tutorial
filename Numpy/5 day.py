6593e0c57cba7905244aaa73e5fc1a0669c96373
Kevin0918 <2473998520@qq.com>
2017-07-07 11:22:30 +0800

    Others

--- Numpy/5 day.py
+++ Numpy/5 day.py
@@ -1,10 +1,15 @@
 # encoding = utf-8
+# 5 day Others
 import numpy as np
 
 
 def main():
-    pass
-
+    print(" FFT : ")  # 傅立叶变换
+    print(np.fft.fft(np.array([1, 1, 1, 1, 1, 1, 1, 1])))
+    print(" Coef : ")  # 相关系数计算
+    print(np.corrcoef([1, 0, 1], [0, 2, 1]))
+    print(" Poly: ")  # 生成一元多次函数
+    print(np.poly1d([2, 1, 3]))
 
 if __name__ == '__main__':
     main()
