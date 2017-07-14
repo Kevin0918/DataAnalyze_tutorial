044bbf4741713c57b32be66f1c5fe41ea7f86457
Kevin0918 <2473998520@qq.com>
2017-07-14 23:52:33 +0800

    Integrade

--- Scipy/Integral.py
+++ Scipy/Integral.py
@@ -1,16 +1,17 @@
 # encoding = utf-8
-# Integral
+# Integral 积分模块
 import numpy as np
 from scipy.integrate import quad, dblquad, nquad  # 引入积分模块
 
 
 def main():
-    print(quad(lambda x: np.exp(-x), 0, np.inf))  # 生成两个数，第二个数是误差范围
-    print(dblquad(lambda t, x: np.exp(-x * t) / t ** 3, 0, np.inf, lambda x: 1, lambda x: np.inf))
+    print(quad(lambda x: np.exp(-x), 0, np.inf))  # 一元积分，生成两个数，第二个数是误差范围
+    print(dblquad(lambda t, x: np.exp(-x * t) / t ** 3, 0, np.inf, lambda x: 1, lambda x: np.inf))  # 二元积分
 
     def f(x, y):
         return x * y
 
+    # 定义x，y的边界：
     def bound_y():
         return [0, 0.5]
 
