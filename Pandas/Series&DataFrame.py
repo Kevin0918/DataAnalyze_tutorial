6e142df860732f53bb21eabd9c10b692667e61d8
Kevin0918 <2473998520@qq.com>
2017-07-16 00:03:04 +0800

    Series&&DataFrame

--- Pandas/Series&DataFrame.py
+++ Pandas/Series&DataFrame.py
@@ -8,10 +8,12 @@ def main():
     # Data Structure
     s = pd.Series([i * 2 for i in range(1, 11)])
     print(type(s))
+
     # DataFrame
     dates = pd.date_range("20170301", periods=8)
     df1 = pd.DataFrame(np.random.randn(8, 5), index=dates, columns=list("ABCDE"))
     print(df1)
+
     df2 = pd.DataFrame({"A": np.array([1, 2, 3, 4]),
                         "B": pd.Timestamp("20170301"),  # 时间序列
                         "C": pd.Series(1, index=list(range(4)), dtype="float32"),
