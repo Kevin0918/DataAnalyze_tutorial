d75916bae650a9b2073bd0f113c03aec7d416901
Kevin0918 <2473998520@qq.com>
2017-07-16 00:01:38 +0800

    Series&&DataFrame

--- Pandas/Series&DataFrame.py
+++ Pandas/Series&DataFrame.py
@@ -12,7 +12,7 @@ def main():
     dates = pd.date_range("20170301", periods=8)
     df1 = pd.DataFrame(np.random.randn(8, 5), index=dates, columns=list("ABCDE"))
     print(df1)
-    df2 = pd.DataFrame({"A": np.array.([1, 2, 3, 4]),
+    df2 = pd.DataFrame({"A": np.array([1, 2, 3, 4]),
                         "B": pd.Timestamp("20170301"),  # 时间序列
                         "C": pd.Series(1, index=list(range(4)), dtype="float32"),
                         "D": np.array([3] * 4, dtype="float32"),
